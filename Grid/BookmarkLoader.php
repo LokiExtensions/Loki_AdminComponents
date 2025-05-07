35661cd7fad984b961dd3ddfff511f46b159d5b4
Jisse Reitsma <jisse@yireo.com>
2025-05-07 08:47:08 +0200

    Simplify loading of grid bookmark

--- Grid/BookmarkLoader.php
+++ Grid/BookmarkLoader.php
@@ -2,43 +2,37 @@
 
 namespace Yireo\LokiAdminComponents\Grid;
 
-use Magento\Framework\Api\SearchCriteriaBuilder;
-use Magento\Ui\Api\BookmarkRepositoryInterface;
-use Magento\Ui\Api\Data\BookmarkInterface;
+use Magento\Ui\Api\BookmarkManagementInterface;
+use Magento\Ui\Api\Data\BookmarkSearchResultsInterface;
+use Magento\Ui\Api\Data\BookmarkSearchResultsInterfaceFactory;
+
 class BookmarkLoader
 {
     public function __construct(
-        private BookmarkRepositoryInterface $bookmarkRepository,
-        private SearchCriteriaBuilder $searchCriteriaBuilder,
+        private BookmarkManagementInterface $bookmarkManagement,
     ) {
     }
 
     public function getBookmarkData(string $namespace): array
     {
-        $this->searchCriteriaBuilder->addFilter('namespace', $namespace);
-        $searchResults = $this->bookmarkRepository->getList($this->searchCriteriaBuilder->create());
-
-        if ($searchResults->getTotalCount() === 0) {
+        /** @var BookmarkSearchResultsInterface $bookmarks */
+        $bookmarks = $this->bookmarkManagement->loadByNamespace($namespace);
+        if ($bookmarks->getTotalCount() < 1) {
             return [];
         }
 
-        foreach ($searchResults->getItems() as $bookmark) {
+        foreach ($bookmarks->getItems() as $bookmark) {
             if ($bookmark->isCurrent()) {
-                return $this->getDataFromBookmark($bookmark);
+                return $bookmark->getConfig();
             }
         }
 
-        foreach ($searchResults->getItems() as $bookmark) {
+        foreach ($bookmarks->getItems() as $bookmark) {
             if ($bookmark->getIdentifier() === 'default') {
-                return $this->getDataFromBookmark($bookmark);
+                return $bookmark->getConfig();
             }
         }
 
         return [];
-    }
-
-    private function getDataFromBookmark(BookmarkInterface $bookmark): array
-    {
-        return $bookmark->getConfig();
     }
 }
