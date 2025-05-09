4bdaff9171b6cd79e240626dc776af8b014f3773
Jisse Reitsma <jisse@yireo.com>
2025-05-09 10:50:40 +0200

    Basic implementation of array and collection handler

--- ProviderHandler/ArrayHandler.php
+++ ProviderHandler/ArrayHandler.php
@@ -3,7 +3,39 @@ declare(strict_types=1);
 
 namespace Yireo\LokiAdminComponents\ProviderHandler;
 
+use Magento\Framework\DataObject;
+use RuntimeException;
+use Yireo\LokiAdminComponents\Grid\State as GridState;
+
 class ArrayHandler implements ProviderHandlerInterface
 {
+    public function getItem($provider, int|string $identifier): DataObject
+    {
+        if (false === is_array($provider)) {
+            throw new RuntimeException('Provider is not an array.');
+        }
 
+        if (isset($provider[$identifier])) {
+            return $provider[$identifier];
+        }
+
+        throw new RuntimeException('Unable to extra identifier "'.$identifier.'" from array');
+    }
+
+    public function getItems($provider, GridState $gridState): array
+    {
+        return $provider;
+    }
+
+    public function saveItem($provider, DataObject $item)
+    {
+    }
+
+    public function deleteItem($provider, DataObject $item)
+    {
+    }
+
+    public function duplicateItem($provider, DataObject $item)
+    {
+    }
 }
--- ProviderHandler/CollectionHandler.php
+++ ProviderHandler/CollectionHandler.php
@@ -3,7 +3,59 @@ declare(strict_types=1);
 
 namespace Yireo\LokiAdminComponents\ProviderHandler;
 
+use Magento\Framework\Data\Collection\AbstractDb;
+use Magento\Framework\DataObject;
+use RuntimeException;
+use Yireo\LokiAdminComponents\Grid\State as GridState;
+
 class CollectionHandler implements ProviderHandlerInterface
 {
 
+    public function getItem($provider, int|string $identifier): DataObject
+    {
+        if (false === $provider instanceof AbstractDb) {
+            throw new RuntimeException('Provider is not an instance of '.AbstractDb::class);
+        }
+
+        $idFieldName = $provider->getResource()->getIdFieldName();
+        $provider->addFieldToFilter($idFieldName, $identifier);
+        $provider->setPageSize(1);
+        $provider->setCurPage(1);
+
+        return $provider->getFirstItem();
+    }
+
+    public function getItems($provider, GridState $gridState): array
+    {
+        if (false === $provider instanceof AbstractDb) {
+            throw new RuntimeException('Provider is not an instance of '.AbstractDb::class);
+        }
+
+        $provider->setPageSize($gridState->getLimit());
+        $provider->setCurPage($gridState->getPage());
+
+        $search = $gridState->getSearch();
+        if (!empty($search)) {
+            foreach ($gridState->getSearchableFields() as $searchableField) {
+                $provider->addFieldToFilter($searchableField, ['like' => '%'.$search.'%']);
+            }
+        }
+
+        return $provider->load();
+    }
+
+    public function saveItem($provider, DataObject $item)
+    {
+        // TODO: Implement saveItem() method.
+    }
+
+    public function deleteItem($provider, DataObject $item)
+    {
+        // TODO: Implement deleteItem() method.
+    }
+
+    public function duplicateItem($provider, DataObject $item)
+    {
+        // TODO: Implement duplicateItem() method.
+    }
 }
