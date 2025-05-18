872584f21d5fa35c89f7240360c70e924854d5f2
Jisse Reitsma <jisse@yireo.com>
2025-05-18 19:44:13 +0200

    Fix redirect after mass action

---
+++ Plugin/RedirectLokiUrlsPlugin.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Yireo\LokiAdminComponents\Plugin;
+
+use Magento\Framework\App\FrontController;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Controller\Result\Redirect as ResultRedirect;
+
+class RedirectLokiUrlsPlugin
+{
+    public function __construct(
+        private RequestInterface $request
+    ) {
+    }
+
+    public function afterDispatch(FrontController $subject, $result)
+    {
+        if (false === $result instanceof ResultRedirect) {
+            return $result;
+        }
+
+        $lokiRedirectUrl = $this->request->getParam('loki_redirect_url');
+        if (empty($lokiRedirectUrl)) {
+            return $result;
+        }
+
+        $result->setUrl($lokiRedirectUrl);
+        return $result;
+    }
+}
