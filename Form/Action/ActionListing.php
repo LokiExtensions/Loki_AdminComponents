352b87e79ed9adfa628cf40b4fd16fd430a7e09c
Jisse Reitsma <jisse@yireo.com>
2025-07-02 16:56:06 +0200

    Add default param

--- Form/Action/ActionListing.php
+++ Form/Action/ActionListing.php
@@ -5,7 +5,7 @@ namespace Yireo\LokiAdminComponents\Form\Action;
 class ActionListing
 {
     public function __construct(
-        private array $actions,
+        private array $actions = [],
     ) {
     }
 
