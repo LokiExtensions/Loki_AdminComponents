aa43101d710460f32d35ab98b06a7592ba0a9a52
Jisse Reitsma <jisse@yireo.com>
2025-10-15 10:52:36 +0200

    Add Column[] type hint to ArrayProviderInterface

--- Provider/ArrayProviderInterface.php
+++ Provider/ArrayProviderInterface.php
@@ -3,8 +3,13 @@ declare(strict_types=1);
 
 namespace Loki\AdminComponents\Provider;
 
+use Loki\AdminComponents\Grid\Column\Column;
+
 interface ArrayProviderInterface
 {
+    /**
+     * @return Column[]
+     */
     public function getColumns(): array;
     public function getData(): array;
 }
