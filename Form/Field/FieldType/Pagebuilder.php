a58b4180bf5f3afd3b41058064d2c3c00cf5c15f
Jisse Reitsma <jisse@yireo.com>
2025-11-25 13:29:28 +0100

    WIP UiComponents

---
+++ Form/Field/FieldType/Pagebuilder.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Loki\AdminComponents\Form\Field\FieldType;
+
+use Loki\AdminComponents\Form\Field\FieldTypeInterface;
+
+class Pagebuilder implements FieldTypeInterface
+{
+    public function getTemplate(): string
+    {
+        return 'Loki_AdminComponents::form/field_type/pagebuilder.phtml';
+    }
+}
