a6bb407b3209c100bc1943c2c55bd9adadafc922
Jisse Reitsma <jisse@yireo.com>
2026-01-21 13:05:28 +0100

    Add new ViewModel EntitySelect

---
+++ ViewModel/Form/Field/EntitySelect.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Loki\AdminComponents\ViewModel\Form\Field;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class EntitySelect implements ArgumentInterface
+{
+
+}
