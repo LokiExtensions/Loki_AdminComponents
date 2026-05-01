312bfd6a9978b216f49e3048fa52d9fd310f10d3
Jisse Reitsma <jisse@yireo.com>
2026-05-01 12:00:51 +0200

    New multiselect field type (even though this already was possible with select)

---
+++ Form/Field/FieldType/Multiselect.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Loki\AdminComponents\Form\Field\FieldType;
+
+use Loki\AdminComponents\Form\Field\Field;
+use Loki\AdminComponents\Form\Field\FieldTypeInterface;
+use Magento\Framework\Data\OptionSourceInterface;
+use Magento\Framework\ObjectManagerInterface;
+
+class Multiselect extends Select
+{
+}
