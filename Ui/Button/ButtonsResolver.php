57f7fc9ab051d3d6e08bf1217ef53a56e95ab06a
Jisse Reitsma <jisse@yireo.com>
2026-07-02 15:50:56 +0200

    style: Refactor ButtonsResolver for cleariy

--- Ui/Button/ButtonsResolver.php
+++ Ui/Button/ButtonsResolver.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace Loki\AdminComponents\Ui\Button;
 
+use Loki\AdminComponents\Component\Form\FormRepository;
+use Loki\AdminComponents\Component\Grid\GridRepository;
 use Loki\AdminComponents\Provider\FormProviderInterface;
 use Magento\Framework\DataObject;
 use Magento\Framework\View\Element\AbstractBlock;
@@ -18,30 +20,54 @@ class ButtonsResolver
      * @return Button[]
      */
     public function resolve(
+        FormRepository|GridRepository $repository,
         AbstractBlock $block,
-        object $provider,
-        mixed $item
     ): array {
+        $provider = $repository->getProvider();
         if ($provider instanceof FormProviderInterface) {
             return $provider->getForm()->getButtons();
         }
 
-        $buttonDefinitions = $block->getButtons();
-        if (!empty($buttonDefinitions)) {
-            $buttons = [];
-            foreach ($buttonDefinitions as $buttonDefinition) {
-                $buttons[] = $this->buttonFactory->create(
-                    method: (string)$buttonDefinition['method'],
-                    label: (string)$buttonDefinition['label'],
-                    cssClass: isset($buttonDefinition['cssClass']) ? (string)$buttonDefinition['cssClass'] : '',
-                    url: isset($buttonDefinition['url']) ? (string)$buttonDefinition['url'] : '',
-                    primary: $buttonDefinition['primary'] ?? false,
-                );
-            }
-
+        $buttons = $this->getButtonsFromBlock($block);
+        if (!empty($buttons)) {
             return $buttons;
         }
 
+        if ($repository instanceof FormRepository) {
+            return $this->getDefaultFormButtons($repository);
+        }
+
+        if ($repository instanceof GridRepository) {
+            return $this->getDefaultGridButtons($repository);
+        }
+
+        return [];
+    }
+
+    private function getButtonsFromBlock(AbstractBlock $block): array
+    {
+        $buttonDefinitions = $block->getButtons();
+        if (empty($buttonDefinitions)) {
+            return [];
+        }
+
+        $buttons = [];
+        foreach ($buttonDefinitions as $buttonDefinition) {
+            $buttons[] = $this->buttonFactory->create(
+                method: (string)$buttonDefinition['method'],
+                label: (string)$buttonDefinition['label'],
+                cssClass: isset($buttonDefinition['cssClass']) ? (string)$buttonDefinition['cssClass'] : '',
+                url: isset($buttonDefinition['url']) ? (string)$buttonDefinition['url'] : '',
+                primary: $buttonDefinition['primary'] ?? false,
+            );
+        }
+
+        return $buttons;
+    }
+
+    private function getDefaultFormButtons(FormRepository $repository): array
+    {
+        $item = $repository->getItem();
         if ($item instanceof DataObject && $item->getId() > 0) {
             return [
                 $this->buttonFactory->createCloseAction(),
@@ -58,5 +84,10 @@ class ButtonsResolver
             // @todo: This looses current changes when creating a new item
             //$this->buttonFactory->createSaveContinueAction(),
         ];
+    }
+
+    private function getDefaultGridButtons(GridRepository $repository): array
+    {
+        return [];
     }
 }
