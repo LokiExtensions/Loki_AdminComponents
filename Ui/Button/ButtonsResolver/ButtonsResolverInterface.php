99cfded034c7508aa9a3326ea23dfb528e610d2f
Jisse Reitsma <jisse@yireo.com>
2026-09-03 20:24:55 +0200

    chore: Add type-hints

--- Ui/Button/ButtonsResolver/ButtonsResolverInterface.php
+++ Ui/Button/ButtonsResolver/ButtonsResolverInterface.php
@@ -3,9 +3,15 @@ declare(strict_types=1);
 
 namespace Loki\AdminComponents\Ui\Button\ButtonsResolver;
 
+use Loki\AdminComponents\Ui\Button\ButtonInterface;
 use Loki\Components\Component\ComponentRepository;
 
 interface ButtonsResolverInterface
 {
+    /**
+     * @param ComponentRepository $repository
+     * @param ButtonInterface[] $buttons
+     * @return ButtonInterface[]
+     */
     public function resolve(ComponentRepository $repository, array $buttons = []): array;
 }
